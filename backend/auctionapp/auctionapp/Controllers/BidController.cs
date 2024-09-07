9551a4630e3720dc96d0af522e73d75fc9e2b7ca
Lyunzh <yunzhi070@gmail.com>
2024-09-07 13:37:30 +0800

    完成用户出价接口

--- backend/auctionapp/auctionapp/Controllers/BidController.cs
+++ backend/auctionapp/auctionapp/Controllers/BidController.cs
@@ -1,68 +1,94 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using auctionapp.Models;
-//using System.IdentityModel.Tokens.Jwt;
-//using Microsoft.IdentityModel.Tokens;
-//using System.Security.Claims;
-//using System.Text;
-//using System.Security.Cryptography;
-//using Microsoft.EntityFrameworkCore.Storage;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using auctionapp.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.Storage;
 
-//namespace auctionapp.Controllers
-//{
-//    [Route("api/bid")]
-//    [ApiController]
+namespace auctionapp.Controllers
+{
+    [Route("api/bid")]
+    [ApiController]
 
-//    public class BidController : Controller
-//    {
-        
-
-       
-//        private readonly AuctionDbContext _context;
-
-//        public BidController(AuctionDbContext context)
-//        {
-//            _context = context;
-//        }
-
-
-//        //用户出价
-
-//        [HttpPost]
-
-//        public async Task<ActionResult<string>> UserBid([FromBody] bidDataDto bidData)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest(new { message = "Invalid input data." });
-//            }
-//            try
-//            {
-//                var auction = await _context.Auctions
-//                    .Include(auction => auction.Bidrecords)
-//                    .FirstOrDefluatAsync(bidData.auctionId)
-
-//            }
-
-//        }
+    public class BidController : Controller
+    {
 
 
 
+        private readonly AuctionDbContext _context;
+
+        public BidController(AuctionDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //用户出价
+
+        [HttpPost]
+
+        public async Task<ActionResult<string>> UserBid([FromBody] bidDataDto bidData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Invalid input data." });
+            }
+            try
+            {
+                var auction = await _context.Auctions
+                    .Include(auction => auction.Bidrecords)
+                    .FirstOrDefaultAsync(auction => auction.Auctionid == bidData.auctionId);
+
+                if (auction == null)
+                {
+                    return BadRequest(new { message = "Invaild Auction!" });
+                }
+
+                var userBid = new Bidrecord
+                {
+                    Bidid = auction.Bidrecords.Any() ? auction.Bidrecords.Max(a => a.Bidid) + 1 : 1,
+                    Auctionid = auction.Auctionid,
+                    Userid = bidData.userId,
+                    Bidamount = bidData.bidAmount,
+                    Bidtime = DateTime.Now,
+                    Auction = auction,
+                    User = await _context.Users.FindAsync(bidData.userId)
+                };
+
+                _context.Bidrecords.AddAsync(userBid);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
 
 
 
-//        public class bidDataDto
-//        {
-//            public decimal auctionId { get; set; }
-
-//            public decimal userId { get; set; }
-
-//            public decimal bidAmount { get; set; }
-
-//            public string bidTime { get; set; }
-//        }
 
 
 
+        public class bidDataDto
+        {
+            public decimal auctionId { get; set; }
 
-//    }
+            public decimal userId { get; set; }
+
+            public decimal bidAmount { get; set; }
+
+            public string bidTime { get; set; }
+        }
+
+
+
+
+    }
+}
